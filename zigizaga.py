d099003d6bc865c227e6d69b4af2571920b35dff
Mash-iano <ianmacharia290@gmail.com>
2025-03-04 00:32:50 +0300

    zigizigi

--- zigizaga.py
+++ zigizaga.py
@@ -1,8 +1,8 @@
 # second impelmentation as a module
-from functions import add, subtract, multiply, divide
-
-sum = add(2,3)
-difference = subtract(2,3)
-product = multiply(2,3)
-division = divide(2,3)
+from functions.functions import add, subtract, multiply, divide
+from functions.numbers import num1 , num2
+sum = add(num1 , num2)
+difference = subtract(num1 , num2)
+product = multiply(num1 , num2)
+division = divide(num1 , num2)
 print(sum,difference,product,division)
